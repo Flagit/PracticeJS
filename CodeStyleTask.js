648bb55f68963844ca8ac2b6778ea55486d56b8d
unknown <Dzianis_Buividovich@EPBYMINW2657.minsk.epam.com>
2015-09-22 15:08:47 +0300

    Fixed

--- CodeStyleTask.js
+++ CodeStyleTask.js
@@ -7,12 +7,12 @@ function pow(x, n){
 
   return result;
 }
-
-x = prompt("x?", '')
-n = prompt("n?", '')
+ 
+var x = prompt("x?", '');
+var n = prompt("n?", '');
 
 if (n <= 0){
-  alert('Степень '+n+'не поддерживается, введите целую степень, большую 0');
+  alert('Степень '+ n +'не поддерживается, введите целую степень, большую 0');
 } else  {
-  alert(pow(x, n))
+  alert(pow(x, n));
 }
